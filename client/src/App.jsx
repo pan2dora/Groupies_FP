a80b1188406b27ddeaf8ee94d26d5018b8afa57b
pan2dora <pan2dora.brown@gmail.com>
2023-05-12 22:17:36 -0400

    fixed routing, switched from main.jsx to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,38 @@
-import './App.css'
-import MyNavBar from './components/Navbar/Navbar'
+import "./App.css";
+import MyNavBar from "./components/Navbar/Navbar";
+import {Route,Routes,} from "react-router-dom";
+import Home from "./components/Routes/Home";
+import Profile from "./components/Routes/Profile";
+import About from "./components/Routes/About";
 
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+   
+//    <Route path="/" element={<MyNavBar/>}>
+//        <Route path="/home" element={<Home />} />
+//       <Route path="/user-profile" element={<Profile />} />
+     
+//     </Route>
+  
+    
+//   )
+// );
 
 function App() {
-
   return (
-    <div className="w-screen h-screen bg-yellow-50">
-       <MyNavBar />
-     
-    </div>
-  )
+    <>
+    <div className="w-screen h-screen bg-yellow-50"></div>
+     <MyNavBar/>
+     <Routes>
+
+    <Route path="/" element = {<Home/>}/>
+    <Route path="/profile" element = {<Profile/>}/>
+    <Route path="/about" element = {<About/>}/>
+    <Route path="/home" element = {<Home/>}/>
+     </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
