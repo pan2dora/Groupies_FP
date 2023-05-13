fa7bfb052619f19c49cc18d298a460bcc96d7339
pan2dora <pan2dora.brown@gmail.com>
2023-05-12 22:16:29 -0400

    cleaned up structure of routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,30 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+
+//Function that takes a element that will create my router
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/"  element={<Home/>} >
+//       <Route path="/user-profile" element={<Profile/>} />
+//     </Route>
+//   )
+// )
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   
+  
+    <BrowserRouter>
     <App />
+</BrowserRouter>
+ 
+    
   </React.StrictMode>,
 )
