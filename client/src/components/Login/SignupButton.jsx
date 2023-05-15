fc08c104af8c5cbcfe6ab1b5a9787c0814d4d2c9
pan2dora <pan2dora.brown@gmail.com>
2023-05-14 21:53:12 -0400

    setup Auth0 endpoint that specifies signup

--- client/src/components/Login/SignupButton.jsx
+++ client/src/components/Login/SignupButton.jsx
@@ -7,8 +7,15 @@ export const SignupButton = () => {
   const handleSignUp = async () => {
     await loginWithRedirect({
       appState: {
-        returnTo: "/",
+        returnTo: "/profile",
         //later navigate to page to edit profile
-      }
-  };
-};
+    },
+    // using and defining this property means that this 
+    //object will be sent as a query to the auth0 endpoint 
+    //letting it know that we wwant the sign up page
+          authorizationParams: {
+            screen_hint: "signup",
+          },
+        });
+      };
+    }
