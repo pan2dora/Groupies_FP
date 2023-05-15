6e182d23db2700e2779ed37e3b490cfb1fc09a1e
pan2dora <pan2dora.brown@gmail.com>
2023-05-15 16:32:18 -0400

    Created functionality to render data based on groups id

--- client/src/components/Routes/Groups/GroupsList.jsx
+++ client/src/components/Routes/Groups/GroupsList.jsx
@@ -1,8 +1,42 @@
- const GroupList =(  ) =>{
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-    return (
-        <p>group</p>
-    )
- }
+const GroupList = () => {
+  const { groupId } = useParams();
+  const [posts, setGroupPosts] = useState([]);
 
- export default GroupList;
+
+
+
+const loadPosts = () => {
+        // A function to fetch the list of students that will be load anytime that list change
+        
+        
+        
+        fetch(`http://localhost:8080/group/${groupId}`)
+            .then((response) => response.json())
+            .then((posts) => {
+                setGroupPosts(posts);
+            });
+    }
+
+    useEffect(() => {
+        loadPosts();
+    }, []);
+  
+ console.log(posts, "This is data ")
+  
+
+  return (
+    <>
+      <h1>Group Posts</h1>
+      {posts.map((post) => (
+        <div key={post.group_post_id}>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default GroupList;
