58e3ac2b02833dcd8f97d6aa1925ebc338e6e64b
pan2dora <pan2dora.brown@gmail.com>
2023-05-15 17:14:23 -0400

    Switched my components around, now my groups functionality is in my Groups component and my post and stylign will be in my Group List component

--- client/src/components/Routes/Groups/Group.jsx
+++ client/src/components/Routes/Groups/Group.jsx
@@ -1,14 +1,38 @@
 import GroupList from "./GroupsList";
-
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 const Group = () => {
-
+    const { groupId } = useParams();
+    const [posts, setGroupPosts] = useState([]);
+  
+  
+  
+  
+  const loadPosts = () => {
+          // A function to fetch the list of students that will be load anytime that list change
+          
+          
+          
+          fetch(`http://localhost:8080/group/${groupId}`)
+              .then((response) => response.json())
+              .then((posts) => {
+                  setGroupPosts(posts);
+              });
+      }
+  
+      useEffect(() => {
+          loadPosts();
+      }, []);
+    
+   console.log(posts, "This is data ")
+    
 
 
     return(
     <>
-    <p>Group page</p>
-    <GroupList/>
+   
+    {groupId && <GroupList posts = {posts}/>}
     </>
 )
 
--- client/src/components/Routes/Groups/GroupsList.jsx
+++ client/src/components/Routes/Groups/GroupsList.jsx
@@ -1,41 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-const GroupList = () => {
-  const { groupId } = useParams();
-  const [posts, setGroupPosts] = useState([]);
-
-
-
-
-const loadPosts = () => {
-        // A function to fetch the list of students that will be load anytime that list change
-        
-        
-        
-        fetch(`http://localhost:8080/group/${groupId}`)
-            .then((response) => response.json())
-            .then((posts) => {
-                setGroupPosts(posts);
-            });
-    }
-
-    useEffect(() => {
-        loadPosts();
-    }, []);
-  
- console.log(posts, "This is data ")
+const GroupList = ({posts}) => {
   
 
   return (
-    <>
-      <h1>Group Posts</h1>
-      {posts.map((post) => (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+     
+      {posts && posts.map((post) => (
         <div key={post.group_post_id}>
           <p>{post.content}</p>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
