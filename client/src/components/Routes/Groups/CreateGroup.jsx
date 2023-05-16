dd165c4e9c6d5b5f663550ced64b16e491d694b0
pan2dora <pan2dora.brown@gmail.com>
2023-05-16 13:00:46 -0400

    Added post request and sucessfully tested it in client -> needs functionality that tells the user if the group has been added

--- client/src/components/Routes/Groups/CreateGroup.jsx
+++ client/src/components/Routes/Groups/CreateGroup.jsx
@@ -4,34 +4,72 @@ import { useState } from "react";
 
 const CreateGroup = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [newGroup, setNewGroup] = useState("");
+
+  const handleNewGroup = (event) => {
+    const group_name = event.target.value;
+    setNewGroup(group_name);
+  };
+
+  const addNewGroup = () => {
+    const group = {
+      group_name: newGroup,
+    };
+
+    fetch(`http://localhost:8080/group`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(group), // Add the body property with the JSON stringified group object
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const createdGroup = data;
+        setNewGroup(createdGroup.group_name);
+      })
+      .catch((error) => {
+        console.error("Error adding group:", error);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addNewGroup();
+  };
 
   return (
     <Modal
       open={modalOpen}
       onClose={() => setModalOpen(false)}
       onOpen={() => setModalOpen(true)}
-      trigger={<Link> Create G </Link>}
-    > 
+      trigger={<Link> Create</Link>}
+    >
       <Modal.Header>Create Group</Modal.Header>
       <Modal.Content>
-      <form >
-
-  <div >
-    <label>Group Name </label>
-    <input type="text" name="last-name" placeholder="Last Name"/>
-  </div>
-  <div >
-    <div class="ui checkbox">
-      <input type="checkbox" tabindex="0" class="hidden"/>
-      <label>I agree to the Terms and Conditions</label>
-    </div>
-  </div>
-  <button  type="submit">Submit</button>
-</form>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Group Name</label>
+            <input
+              type="text"
+              value={newGroup}
+              required
+              placeholder="Name your group"
+              onChange={handleNewGroup}
+            />
+          </div>
+          <div>
+            <div className="ui checkbox">
+              <input type="checkbox" tabIndex="0" className="hidden" />
+              <label>I agree to the Terms and Conditions</label>
+            </div>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </Modal.Content>
       <Modal.Actions></Modal.Actions>
     </Modal>
   );
 };
+
+
 
 export default CreateGroup;
