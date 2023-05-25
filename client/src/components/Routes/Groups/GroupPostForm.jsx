373b61d4224032162f8db9589b647385ea0639a5
pan2dora <pan2dora.brown@gmail.com>
2023-05-24 20:20:59 -0400

    added call to giphy api that allows users to search for gif and include them in their post

--- client/src/components/Routes/Groups/GroupPostForm.jsx
+++ client/src/components/Routes/Groups/GroupPostForm.jsx
@@ -1,73 +1,151 @@
 import React, { useState } from "react";
-import { Button, Form, Input } from "semantic-ui-react";
+import { Button, Form, Input, Modal, Card, Image } from "semantic-ui-react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const GroupPostForm = ({ onSavePost, groupId }) => {
   const [newPost, setNewPost] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const { isAuthenticated, user } = useAuth0();
+  const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
 
-  const handleAddPost = (event) => {
-    const content = event.target.value;
-    setNewPost(content);
-  };
-
-  const addNewPost = () => {
-    const post = {
-      content: newPost,
-      userId: user.sub,
-    };
-
-    fetch(`/api/group/${groupId}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(post),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        onSavePost(data);
-        setNewPost("");
-      })
-      .catch((error) => {
-        console.error("Error adding post:", error);
-      });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (isAuthenticated && groupId) {
-      addNewPost();
-    }
-  };
-
-  return (
-    <div>
-      {isAuthenticated && groupId ? (
-        <Form onSubmit={handleSubmit}>
-          <Form.Field>
-            <Input
-              type="text"
-              value={newPost}
-              required
-              placeholder="What's on your mind?"
-              onChange={handleAddPost}
-            />
-          </Form.Field>
-          <Button type="submit" disabled={!groupId}>
-            {groupId ? "Post" : "Not a member"}
-          </Button>
-        </Form>
-      ) : (
-        <div>
-          {!isAuthenticated ? (
-            <p>Please log in to add a post.</p>
-          ) : (
-            <p>You are not a member of this group.</p>
-          )}
-        </div>
-      )}
-    </div>
-  );
+ const handleAddPost = (event) => {
+  const content = event.target.value;
+  setNewPost(content);
 };
 
-export default GroupPostForm;
+const handleModalOpen = () => {
+  setModalOpen(true);
+};
 
+const handleModalClose = () => {
+  setModalOpen(false);
+};
+
+const handleSearchChange = (event) => {
+  const query = event.target.value;
+  setSearchQuery(query);
+};
+
+const handleSearchGifs = (event) => {
+  event.preventDefault();
+  const limit = 5;
+
+  // Perform Giphy API search request
+  fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&limit=${limit}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Extract the GIF URLs from the API response
+      const results = data.data.map((gif) => ({
+        id: gif.id,
+        gif: gif.images.fixed_height,
+      }));
+
+      setSearchResults(results);
+    })
+    .catch((error) => {
+      console.error("Error searching GIFs:", error);
+    });
+};
+
+const handleAddGif = (gifUrl) => {
+  const gifInput = `<img src="${gifUrl.gif.url}" alt="GIF" />`;
+  const updatedPost = newPost ;
+  setNewPost(updatedPost);
+};
+
+const addNewPost = () => {
+  const post = {
+    content: newPost,
+    userId: user.sub,
+    image: searchResults[0]?.gif.url || null,
+  };
+
+  fetch(`/api/group/${groupId}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(post),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      onSavePost(data);
+      setNewPost("");
+    })
+    .catch((error) => {
+      console.error("Error adding post:", error);
+    });
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  if (isAuthenticated && groupId) {
+    addNewPost();
+  }
+};
+
+return (
+  <div>
+    <Modal
+      trigger={
+        <Button onClick={handleModalOpen} fluid>
+          {newPost ? newPost : "What's on your mind?"}
+        </Button>
+      }
+      open={modalOpen}
+      onClose={handleModalClose}
+      size="small"
+    >
+      <Modal.Header>Create Post</Modal.Header>
+      <Modal.Content>
+        <Form onSubmit={handleSubmit}>
+          <Form.TextArea
+            value={newPost}
+            required
+            placeholder="What's on your mind?"
+            onChange={handleAddPost}
+          />
+          <Form.Field>
+            <Input
+              icon="search"
+              placeholder="Search GIFs"
+              value={searchQuery}
+              onChange={handleSearchChange}
+            />
+          </Form.Field>
+          <Button onClick={handleSearchGifs}>Search</Button>
+          {searchResults.length > 0 && (
+            <Card.Group itemsPerRow={4}>
+              {searchResults.map((result) => (
+                <Card key={result.id}>
+                  <Image src={result.gif.url} alt="GIF" />
+                  <Card.Content extra>
+                    <Button
+                      fluid
+                      basic
+                      color="blue"
+                      onClick={() => handleAddGif(result)}
+                    >
+                      Add
+                    </Button>
+                  </Card.Content>
+                </Card>
+              ))}
+            </Card.Group>
+          )}
+        </Form>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={handleModalClose}>Cancel</Button>
+        <Button
+          positive
+          onClick={handleSubmit}
+          disabled={!groupId || !newPost}
+        >
+          {groupId ? "Post" : "Not a member"}
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  </div>
+);}
+export default GroupPostForm;
