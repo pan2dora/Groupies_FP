0eafd9d8375de6a7bf19fbeef4bf77d536527fc6
pan2dora <pan2dora.brown@gmail.com>
2023-05-24 21:02:04 -0400

    added selected gif state to store the selected gif

--- client/src/components/Routes/Groups/GroupPostForm.jsx
+++ client/src/components/Routes/Groups/GroupPostForm.jsx
@@ -7,145 +7,147 @@ const GroupPostForm = ({ onSavePost, groupId }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedGif, setSelectedGif] = useState(null);
   const { isAuthenticated, user } = useAuth0();
   const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
 
- const handleAddPost = (event) => {
-  const content = event.target.value;
-  setNewPost(content);
-};
-
-const handleModalOpen = () => {
-  setModalOpen(true);
-};
-
-const handleModalClose = () => {
-  setModalOpen(false);
-};
-
-const handleSearchChange = (event) => {
-  const query = event.target.value;
-  setSearchQuery(query);
-};
-
-const handleSearchGifs = (event) => {
-  event.preventDefault();
-  const limit = 5;
-
-  // Perform Giphy API search request
-  fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&limit=${limit}`)
-    .then((response) => response.json())
-    .then((data) => {
-      // Extract the GIF URLs from the API response
-      const results = data.data.map((gif) => ({
-        id: gif.id,
-        gif: gif.images.fixed_height,
-      }));
-
-      setSearchResults(results);
-    })
-    .catch((error) => {
-      console.error("Error searching GIFs:", error);
-    });
-};
-
-const handleAddGif = (gifUrl) => {
-  const gifInput = `<img src="${gifUrl.gif.url}" alt="GIF" />`;
-  const updatedPost = newPost ;
-  setNewPost(updatedPost);
-};
-
-const addNewPost = () => {
-  const post = {
-    content: newPost,
-    userId: user.sub,
-    image: searchResults[0]?.gif.url || null,
+  const handleAddPost = (event) => {
+    const content = event.target.value;
+    setNewPost(content);
   };
 
-  fetch(`/api/group/${groupId}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(post),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      onSavePost(data);
-      setNewPost("");
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleSearchChange = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+  };
+
+  const handleSearchGifs = (event) => {
+    event.preventDefault();
+    const limit = 6;
+
+    // Perform Giphy API search request
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&limit=${limit}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract the GIF URLs from the API response
+        const results = data.data.map((gif) => ({
+          id: gif.id,
+          gif: gif.images.fixed_height,
+        }));
+
+        setSearchResults(results);
+      })
+      .catch((error) => {
+        console.error("Error searching GIFs:", error);
+      });
+  };
+
+  const handleAddGif = (result) => {
+    const gifUrl = result.gif.url;
+    setSelectedGif(gifUrl);
+  };
+
+  const addNewPost = () => {
+    const post = {
+      content: newPost,
+      userId: user.sub,
+      image: selectedGif || null,
+    };
+
+    fetch(`/api/group/${groupId}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(post),
     })
-    .catch((error) => {
-      console.error("Error adding post:", error);
-    });
-};
+      .then((response) => response.json())
+      .then((data) => {
+        onSavePost(data);
+        setNewPost("");
+      })
+      .catch((error) => {
+        console.error("Error adding post:", error);
+      });
+  };
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  if (isAuthenticated && groupId) {
-    addNewPost();
-  }
-};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isAuthenticated && groupId) {
+      addNewPost();
+    }
+  };
 
-return (
-  <div>
-    <Modal
-      trigger={
-        <Button onClick={handleModalOpen} fluid>
-          {newPost ? newPost : "What's on your mind?"}
-        </Button>
-      }
-      open={modalOpen}
-      onClose={handleModalClose}
-      size="small"
-    >
-      <Modal.Header>Create Post</Modal.Header>
-      <Modal.Content>
-        <Form onSubmit={handleSubmit}>
-          <Form.TextArea
-            value={newPost}
-            required
-            placeholder="What's on your mind?"
-            onChange={handleAddPost}
-          />
-          <Form.Field>
-            <Input
-              icon="search"
-              placeholder="Search GIFs"
-              value={searchQuery}
-              onChange={handleSearchChange}
+  return (
+    <div>
+      <Modal
+        trigger={
+          <Button onClick={handleModalOpen} fluid>
+            {newPost ? newPost : "What's on your mind?"}
+          </Button>
+        }
+        open={modalOpen}
+        onClose={handleModalClose}
+        size="small"
+      >
+        <Modal.Header>Create Post</Modal.Header>
+        <Modal.Content>
+          <Form onSubmit={handleSubmit}>
+            <Form.TextArea
+              value={newPost}
+              required
+              placeholder="What's on your mind?"
+              onChange={handleAddPost}
             />
-          </Form.Field>
-          <Button onClick={handleSearchGifs}>Search</Button>
-          {searchResults.length > 0 && (
-            <Card.Group itemsPerRow={4}>
-              {searchResults.map((result) => (
-                <Card key={result.id}>
-                  <Image src={result.gif.url} alt="GIF" />
-                  <Card.Content extra>
-                    <Button
-                      fluid
-                      basic
-                      color="blue"
-                      onClick={() => handleAddGif(result)}
-                    >
-                      Add
-                    </Button>
-                  </Card.Content>
-                </Card>
-              ))}
-            </Card.Group>
-          )}
-        </Form>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button onClick={handleModalClose}>Cancel</Button>
-        <Button
-          positive
-          onClick={handleSubmit}
-          disabled={!groupId || !newPost}
-        >
-          {groupId ? "Post" : "Not a member"}
-        </Button>
-      </Modal.Actions>
-    </Modal>
-  </div>
-);}
+            <Form.Field>
+              <Input
+                icon="search"
+                placeholder="Search GIFs"
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+            </Form.Field>
+            <Button onClick={handleSearchGifs}>Search</Button>
+            {searchResults.length > 0 && (
+              <Card.Group itemsPerRow={4}>
+                {searchResults.map((result) => (
+                  <Card key={result.id}>
+                    <Image src={result.gif.url} alt="GIF" />
+                    <Card.Content extra>
+                      <Button
+                        fluid
+                        basic
+                        color="blue"
+                        onClick={() => handleAddGif(result)}
+                      >
+                        Add
+                      </Button>
+                    </Card.Content>
+                  </Card>
+                ))}
+              </Card.Group>
+            )}
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={handleModalClose}>Cancel</Button>
+          <Button
+            positive
+            onClick={handleSubmit}
+            disabled={!groupId || !newPost}
+          >
+            {groupId ? "Post" : "Not a member"}
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </div>
+  );
+};
+
 export default GroupPostForm;
