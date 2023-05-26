18e827f4d1f298638b444aa878f4ddb25d9adfb3
pan2dora <pan2dora.brown@gmail.com>
2023-05-26 15:15:22 -0400

    added test

--- client/vitest.config.js
+++ client/vitest.config.js
@@ -23,3 +23,33 @@ export default defineConfig({
     environment: "jsdom",
   },
 });
+
+
+import { render, screen, fireEvent } from '@testing-library/react';
+import UserSignup from './UserSignup';
+
+describe('UserSignup', () => {
+  it('updates user data on form submission', async () => {
+    render(<UserSignup />);
+
+    const displayNameInput = screen.getByLabelText('Display Name:');
+    const pronounsInput = screen.getByLabelText('Pronouns:');
+    const dateOfBirthInput = screen.getByLabelText('Date of Birth:');
+    const pictureInput = screen.getByLabelText('Picture:');
+    const submitButton = screen.getByRole('button', { name: 'Submit' });
+
+    fireEvent.change(displayNameInput, { target: { value: 'John Doe' } });
+    fireEvent.change(pronounsInput, { target: { value: 'he/him' } });
+    fireEvent.change(dateOfBirthInput, { target: { value: '1990-01-01' } });
+    fireEvent.change(pictureInput, { target: { value: 'https://example.com/picture.jpg' } });
+
+    fireEvent.click(submitButton);
+
+    await screen.findByText('User data updated successfully');
+
+    expect(displayNameInput.value).toBe('');
+    expect(pronounsInput.value).toBe('');
+    expect(dateOfBirthInput.value).toBe('');
+    expect(pictureInput.value).toBe('');
+  });
+});
