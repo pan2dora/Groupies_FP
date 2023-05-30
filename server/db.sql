b1dfa17b1d1cf8c27d02d2a0ac8eca31030d4db3
pan2dora <pan2dora.brown@gmail.com>
2023-05-30 09:23:03 -0400

    fixed db

--- server/db.sql
+++ server/db.sql
@@ -242,20 +242,12 @@ ALTER TABLE ONLY public.group_table ALTER COLUMN group_table_id SET DEFAULT next
 -- Data for Name: group_membership; Type: TABLE DATA; Schema: public; Owner: panpan
 --
 
-INSERT INTO public.group_membership (gtid, uid, is_admin) VALUES (94, 11, NULL);
-INSERT INTO public.group_membership (gtid, uid, is_admin) VALUES (95, 11, true);
 
 
 --
 -- Data for Name: group_post; Type: TABLE DATA; Schema: public; Owner: panpan
 --
 
-INSERT INTO public.group_post (group_post_id, image, content, user_id, group_table_id) VALUES (237, 'https://media2.giphy.com/media/gw3IWyGkC0rsazTi/200.gif?cid=d168aec0bliprrgsvk9hqegvjxelltm5ez9ok25oi68818q5&ep=v1_gifs_search&rid=200.gif&ct=g', 'Test', 11, 93);
-INSERT INTO public.group_post (group_post_id, image, content, user_id, group_table_id) VALUES (238, 'https://media4.giphy.com/media/3ohc0ZRliclUMt98Va/200.gif?cid=d168aec0pxza7fgpd7hpcv5dka08gu94qsq3hu22b5okc0e0&ep=v1_gifs_search&rid=200.gif&ct=g', 'Test', 11, 94);
-INSERT INTO public.group_post (group_post_id, image, content, user_id, group_table_id) VALUES (239, 'https://media4.giphy.com/media/hVTouq08miyVo1a21m/200.gif?cid=d168aec0uuwbn0gtahoubih6k27ldj3kd13e97m9cmovrdk6&ep=v1_gifs_search&rid=200.gif&ct=g', 'Test', 11, 94);
-INSERT INTO public.group_post (group_post_id, image, content, user_id, group_table_id) VALUES (240, 'https://media0.giphy.com/media/l3fzM2wgd6TygHbYA/200.gif?cid=d168aec0bliprrgsvk9hqegvjxelltm5ez9ok25oi68818q5&ep=v1_gifs_search&rid=200.gif&ct=g', 'Test', 11, 94);
-INSERT INTO public.group_post (group_post_id, image, content, user_id, group_table_id) VALUES (241, NULL, 'Test', 11, 94);
-INSERT INTO public.group_post (group_post_id, image, content, user_id, group_table_id) VALUES (242, 'https://media2.giphy.com/media/l3V0H7bYv5Ml5TOfu/200.gif?cid=d168aec0bliprrgsvk9hqegvjxelltm5ez9ok25oi68818q5&ep=v1_gifs_search&rid=200.gif&ct=g', 'TEd', 11, 94);
 
 
 --
@@ -282,29 +274,6 @@ INSERT INTO public.group_table (group_table_id, group_name, description) VALUES 
 -- Data for Name: user_table; Type: TABLE DATA; Schema: public; Owner: panpan
 --
 
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (53, 'p', 'p', 'p', NULL, 'https://s.gravatar.com/avatar/5e586d3f8030175f5524a308356917cf?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fpa.png', 'pan2dora.brown+signuptest@gmail.com', 'p', 'auth0|646a7d5192e83c2d192f4312');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (11, 'Pandora', 'Pandora', 'They/Them', '2023-05-05 00:00:00', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdZnsb9WJSRlXfRpnJ26aRDMxw8S0F1BPFM1i-kn14uA&s', 'pan2dora.brown@gmail.com', NULL, 'google-oauth2|110151895661426733698');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (51, NULL, 'Sandy', 'she/her', '2023-05-26 00:00:00', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2015%2F07%2Fhello-kitty.jpg&q=60', 'pan2dora.brown+test20@gmail.com', NULL, 'auth0|646a7a1529c87bf8f14fb4cd');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (50, NULL, 'John Doe', 'he/him', '1990-01-01 00:00:00', 'https://example.com/profile.jpg', 'pan2dora.brown+test9@gmail.com', NULL, 'auth0|646a79a9d70fc3a64b092810');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (54, NULL, NULL, NULL, NULL, 'https://s.gravatar.com/avatar/ec1e7cc6f35206231770f980cf34b8b0?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fpa.png', 'pan2dora.brown+anothertest@gmail.com', NULL, 'auth0|646b7702d70fc3a64b094a0f');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (55, NULL, NULL, NULL, NULL, 'https://s.gravatar.com/avatar/bbe4af030600d5ccacb081d60a32f395?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fpa.png', 'pan2dora.brown+test202@gmail.com', NULL, 'auth0|646ba630d2af3a84da91dc6f');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (56, NULL, NULL, NULL, NULL, 'https://s.gravatar.com/avatar/e1bc32da6e7044205251778a53019634?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fpa.png', 'pan2dora.brown+rubytest@gmail.com', NULL, 'auth0|646bb032d2af3a84da91de84');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (57, NULL, 'test', 'test', '2023-05-27 00:00:00', 'test', 'testing@gmail.com', NULL, 'auth0|646bb14bc5e194bb12b1d253');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (58, NULL, 'vixenxloki', 'they/them', '2023-05-30 00:00:00', 'test', 'test@test.com', NULL, 'auth0|646be05e2b851c3fe65fa3a0');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (59, NULL, 'vixenxloki', 'they/them', '2023-05-16 00:00:00', 'test', 'pan2dora.brown+test100@gmail.com', NULL, 'auth0|646f57584143421ed3474065');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (60, NULL, NULL, NULL, NULL, 'https://s.gravatar.com/avatar/76386fa0a93962366aed304da1494c8e?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fpa.png', 'pan2dora.brown+test900@gmail.com', NULL, 'auth0|646fad1a2da97822aabcde85');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (10, 'John', NULL, NULL, NULL, 'https://example.com/profile.jpg', 'test1@example.com', NULL, '10');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (9, 'John', NULL, NULL, NULL, 'https://example.com/profile.jpg', 'test@example.com', NULL, '9');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (8, 'Pandora', 'panda_panda', 'they/them', '1993-08-23 00:00:00', 'image will go here', 'blah@example.com', NULL, '8');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (7, 'Yoshi', 'spotted_egg', 'they/them', '1990-10-14 00:00:00', 'image will go here', 'yoshi@example.com', NULL, '7');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (6, 'Toad', 'todally_toad', 'they/them', '1985-09-13 00:00:00', 'image will go here', 'toad@example.com', NULL, '6');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (5, 'Donkey', 'DK81', 'he/him', '1981-07-09 00:00:00', 'image will go here', 'donkeykong@example.com', NULL, '5');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (4, 'Bowser', 'koopa-king', 'he/him', '1985-09-13 00:00:00', 'image will go here', 'bowser@example.com', NULL, '4');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (3, 'Luigi', 'luigixbrave', 'he/they', '1964-11-11 00:00:00', 'image will go here', 'luigi@example.com', NULL, '3');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (2, 'Peach', 'peaches_peaches', 'she/her', '1965-08-01 00:00:00', 'image will go here', 'princesspeach@example.com', NULL, '2');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (1, 'Mario', 'peach_lover', 'he/him', '1964-11-11 00:00:00', 'image will go here', 'mario@example.com', NULL, '1');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (52, NULL, NULL, NULL, NULL, 'https://s.gravatar.com/avatar/a7fff5fffac30c429f5f768716e7a18e?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fpa.png', 'pan2dora.brown+test21@gmail.com', NULL, 'auth0|646a7bcfc3f3a8e08ad02b5c');
-INSERT INTO public.user_table (user_id, given_name, displayname, pronouns, date_of_birth, picture, email, password, auth0_sub) VALUES (28, NULL, NULL, NULL, NULL, NULL, 'pan2dora.brown+test6@gmail.com', NULL, 'auth0|646a2edf29c87bf8f14faa09');
 
 
 --
